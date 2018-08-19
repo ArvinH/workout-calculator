1e5178b392ba8e5814ce9b66997988390998bf8f
黃首翰（ArvinH） <arvin0731@gmail.com>
2018-08-19 15:48:43 +0800

    test(prod): test gh-pages

--- webpack.prod.js
+++ webpack.prod.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'js/[name].bundle.js'
+    filename: 'js/[name].[hash].bundle.js'
   },
   devtool: 'source-map',
   resolve: {
